65b43b3b80c9f22a45473f03d5b6eeb75a497397
BordaAgustin <agustinborda1998@gmail.com>
2020-10-06 12:07:48 -0300

    fixed segfault

--- src/table.c
+++ src/table.c
@@ -27,8 +27,8 @@ int insert(nodoL* head, dato d) {
 	if(!exist(head, d.name)) {
 		nodoL* aux = malloc(sizeof(nodoL));
 		aux -> info = d;
-		aux -> sig = head -> sig -> sig;
-		head -> sig = aux;
+		aux -> sig = head -> sig;
+		head ->sig = aux;
 		return 1;
 	}
 	return 0;
@@ -37,13 +37,13 @@ int insert(nodoL* head, dato d) {
 void show(nodoL* head) {
 	nodoL* aux = head -> sig;
 	while(aux != NULL) {
-		printf("Type: %d\nName: %s\nValue: %i",aux->info.type,aux->info.name,aux->info.value);
+		printf("Type: %d\nName: %s\nValue: %i\n",aux->info.type,aux->info.name,aux->info.value);
 		aux = aux -> sig;
 	}
 }
 
 int delete_node(nodoL* head, char* var) {
-	nodoL* aux = head -> sig;
+	nodoL* aux = head;
 	while(aux->sig!=NULL) {
 		if(strcmp(var,aux->sig->info.name)==0) {
 			nodoL* aux2 = aux ->sig;
@@ -55,8 +55,6 @@ int delete_node(nodoL* head, char* var) {
 	}
 	return 0;
 }
-
-
 
 
 
