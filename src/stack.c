5ac33fd3ae56fa7db8572f99f511d9d1ca43d14c
BordaAgustin <agustinborda1998@gmail.com>
2020-10-07 10:19:10 -0300

    pop now frees the list

--- src/stack.c
+++ src/stack.c
@@ -27,12 +27,11 @@ void push(struct stack_node** p) {
 		*p = p2;
 }
 
-nodoL* pop(struct stack_node** p) {
+void pop(struct stack_node** p) {
 	stack_node* p2 = *p;
 	*p = (*p) -> sig;
-	nodoL* ret_val = p2->list;
+	empty_list(&(p2->list));
 	free(p2);
-	return ret_val;
 }
 
 nodoL* peek(struct stack_node* p) {
