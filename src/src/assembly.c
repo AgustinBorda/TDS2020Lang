faa75edefac049a06494ae929d177540aaec42cd
BordaAgustin <agustinborda1998@gmail.com>
2020-11-02 12:06:24 -0300

    assembly name resolver

--- src/src/assembly.c
+++ src/src/assembly.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 //#include "../headers/assembly.h"
 #include "../headers/table.h"
 
@@ -23,6 +25,42 @@ int assemble(list* l) {
 	write(f, l);
 	return 0;
 }
+//TODO: Move this to a "utils" file.
+int int_length(int n) {
+	if(n == 0) {
+		return 1;
+	}
+	return floor(log10(abs(n))) + 1;
+}
+//
+char* resolve_assembly_name(dato_tree* curr) {
+	char* res;
+	switch (curr -> flag) {
+		case 0: if(curr -> data -> flag == GLOBAL_VAR) {
+				res = malloc(strlen(curr->data->name)+6);
+				sprintf(res,"%s(%rip)", curr->data->name);
+				return res;
+			}
+			else {
+		       		res = malloc(int_length(curr->offset)+6);
+				sprintf(res,"%d(%rbp)",curr-> offset);
+				return res;
+			}
+			break;
+		case 1: 
+			res = malloc(int_length(curr->value)+1);
+			sprintf(res,"$%d",curr->value);
+			return res;
+			break;
+		default:
+			res = malloc(int_length(curr->offset)+6);
+			sprintf(res,"%d(%rbp)",curr-> offset);    	
+			return res;
+	}
+	return NULL;
+}	
+
+
 //{ASSIGN, ADD, SUB, MULT, AND, NOT, RET, INIT_FUN, END_FUN, EQ}
 void write(FILE* f, list* l) {  
 f =  fopen("assembly.txt", "a");      
@@ -30,9 +68,10 @@ f =  fopen("assembly.txt", "a");
 		three_address_code* curr = get(l,i);
 		switch(curr->opcode) {
 			case 0 :
-					fprintf(f, "movl $%d, %%eax\n", curr->dest->value );
-				    fprintf(f, "movl %%eax, %d(%rbp)\n", curr->dest->offset);  
-
+				char* op1_name = resolve_assembly_name(curr -> op1);
+				char* dest_name = resolve_assembly_name(curr->dest);
+				fprintf(f, "movl %s, %%eax\n", op1_name);
+				fprintf(f, "movl %%eax, %s\n", dest_name); //dest_name = -24(rbp) 
 				 break;
 			case 1 : switch(curr -> op1 -> flag){
 					 case 0 : switch(curr -> op2 -> flag){
---
+++ test.c
@@ -0,0 +1,9 @@
+int a;
+long b;
+int main() {
+	int c;
+	c = 5;
+	a = 5;
+	b = c;
+	return 0;
+}
